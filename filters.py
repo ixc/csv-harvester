438d7ce41f4eb3a01b140c95a7c228ff04d373c2
Aram Dulyan <aram00@gmail.com>
2010-08-03 13:02:48 +1000

    Added a function caller helper filter.

--- filters.py
+++ filters.py
@@ -17,3 +17,20 @@ def method(name, *args, **kwargs):
 	def methodcaller(value):
 		return getattr(value, name)(*args, **kwargs)
 	return methodcaller
+
+# A function caller that calls the given function with the arguments specified
+# and the data to parse as either the first argument, or as the argument defined
+# by an "attr_index" keyword argument
+def function(func, *args, **kwargs):
+	def funcaller(value):
+		idx = kwargs.pop('attr_index', 0)
+		if isinstance(idx, int):
+			lst = list(args)
+			lst.insert(idx, value)
+			args = tuple(lst)
+		elif isinstance(idx, str):
+			kwargs[idx] = value
+		else:
+			raise AttributeError('The "attr_index" argument must be either an integer or string; was %s.' % type(idx))
+		return func(*args, **kwargs)
+	return funcaller
